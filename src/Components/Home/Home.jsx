317939e382a5387eaf28e91346a76441dd186265
orpon95 <mdorpon95@gmail.com>
2023-12-21 22:31:05 +0600

    added to button to delete list

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,49 +4,51 @@ import Swal from 'sweetalert2';
 import TodoRow from '../TodoRow/TodoRow';
 import { useQuery } from '@tanstack/react-query';
 // import { withAxios } from 'react-axios';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider, useDrop } from 'react-dnd'
 
 const Home = () => {
 
     const axiosPublic = UseaxiosPublic()
-    // const [datas, setdata] = useState([])
+    const [listdata, setdata] = useState([])
     // console.log(datas, 'datas');
-    const handlesubmit = (e)=>{
+    const handlesubmit = (e) => {
         // e.preventDefault()
         const form = e.target
         const list = form.list.value
-        console.log(list,"list");
+        console.log(list, "list");
 
-        const lists = { list}
-        
- 
+        const lists = { list }
+
+
 
 
         axiosPublic.post("/v1/lists", lists)
-        .then(res => {
-            console.log("response", res.data);
-            // console.log("insertedid",res.data.result.insertedId);
-            // console.log("timestamp",res.data.timestamp);
+            .then(res => {
+                console.log("response", res.data);
+                // console.log("insertedid",res.data.result.insertedId);
+                // console.log("timestamp",res.data.timestamp);
 
-            if (res.data.insertedId) {
-                Swal.fire({
-                    title: 'success!',
-                    text: 'surveys added successfully',
-                    icon: 'success',
-                    confirmButtonText: 'Cool'
-                })
-            }
-            else {
-                Swal.fire({
-                    title: 'error!',
-                    text: 'something wrong ,pls try again',
-                    icon: 'error',
-                    confirmButtonText: 'Cool'
-                })
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        title: 'success!',
+                        text: 'surveys added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+                else {
+                    Swal.fire({
+                        title: 'error!',
+                        text: 'something wrong ,pls try again',
+                        icon: 'error',
+                        confirmButtonText: 'Cool'
+                    })
 
 
-            }
+                }
 
-        })
+            })
 
 
         // axiosPublic.get("/v1/listsInfo")
@@ -60,7 +62,7 @@ const Home = () => {
 
     }
 
-    const { data :datas, isLoading, isFetching, isPending, refetch } = useQuery({
+    const { data: datas, isLoading, isFetching, isPending, refetch } = useQuery({
         queryKey: ["listinfo"],
         queryFn: async () => {
             const res = await axiosPublic.get('/v1/listsInfo')
@@ -69,58 +71,79 @@ const Home = () => {
 
             return await res.data
         }
-        
-        
+
+
 
 
     })
-    console.log(datas,"datas")
+    console.log(datas, "datas")
+    // // dnd
+    // const [{ isOver }, drop] = useDrop(
+    //     () => ({
+    //       accept: "task",
+    //       drop: datas?.map((item)=> { addotemTSection(item._id)}),
+    //       collect: (monitor) => ({
+    //         isOver: !!monitor.isDragging()
+    //       })
+    //     }),
+    //     []
+    //   )
+    //   const addotemTSection =(id)=>{
+    //     console.log(id,"dropped id");
+    //   }
    
-    
     return (
-        <div >
-            {/* input div */}
-            <div className='text-center'>
+        <>
+            <DndProvider backend={HTML5Backend}>
+                <div  >
+                    {/* input div */}
+                    <div className='text-center'>
 
-                <form onSubmit={handlesubmit} action="">
-                    <input type="text" placeholder="Type here" name='list' className="input input-bordered input-accent w-full max-w-xs" />
-                    <input type="submit" value="Create" className='btn bg-cyan-400' />
+                        <form onSubmit={handlesubmit} action="">
+                            <input type="text" placeholder="Type here" name='list' className="input input-bordered input-accent w-full max-w-xs" />
+                            <input type="submit" value="Create" className='btn bg-cyan-400' />
 
-                </form>
+                        </form>
 
 
 
-            </div>
-            {/* list div */}
-            <div className=' flex h-[70vh] gap-10 my-6 '>
+                    </div>
+                    {/* list div */}
+                    <div className=' flex h-[70vh] gap-10 my-6 '>
 
-                {/* to do  div */}
+                        {/* to do  div */}
 
-                <div className='border-2 w-[50%] h-full '>
-                    {
-                        datas?.map((data)=> <TodoRow key={data._id} data={data} ></TodoRow> )
-                    }
+                        <div className='border-2 w-[50%] h-full '>
+                            {
+                                datas?.map((data) => <TodoRow key={data._id} data={data} ></TodoRow>)
+                            }
 
+
+                        </div>
+                        {/* progress div */}
+
+                        <div className='border-2 w-[50%] h-full '>
+                            hi
+
+
+                        </div>
+                        {/* completed div */}
+                        <div className='border-2 w-[50%] h-full '>
+                            hello
+
+
+                        </div>
+
+
+                    </div>
 
                 </div>
-                {/* progress div */}
 
-                <div className='border-2 w-[50%] h-full '>
-                    hi
+            </DndProvider>
 
 
-                </div>
-                {/* completed div */}
-                <div  className='border-2 w-[50%] h-full '>
-                    hello
+        </>
 
-
-                </div>
-
-
-            </div>
-
-        </div>
     );
 };
 
--- src/Components/TodoRow/TodoRow.jsx
+++ src/Components/TodoRow/TodoRow.jsx
@@ -1,14 +1,36 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useDrag, useDrop } from 'react-dnd'
+import { MdDelete } from "react-icons/md";
 
-const TodoRow = ({data}) => {
-    console.log(data,"todo data");
-    const {list,_id} = data
+
+const TodoRow = ({ data, moveItem }) => {
+    console.log(data, "todo data");
+    const { list, _id } = data
+    // dnd
+    const [{ isDragging }, dragRef] = useDrag(
+        () => ({
+            type: "task",
+            item: { id: _id },
+            collect: (monitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        }),
+        []
+    )
+    console.log(isDragging, "dragging");
+
+
 
     return (
-        <div  className='space-y-4 py-7'>
-            <p className='border-2 bg-slate-500 p-4 w-[80%] mx-auto rounded-lg' draggable> {list} </p>
-            
+        <div ref={dragRef} className='space-y-4 py-7'>
+            <p className={` flex items-center justify-center text-white border-2 bg-slate-500 p-4 w-[80%] mx-auto rounded-lg ${isDragging ? "bg-red-400" : "bg-slate-600"} `} >
+                
+                
+                 {list} <MdDelete className='text-red-500 text-3xl' />
+                
+            </p>
+
         </div>
     );
 };
